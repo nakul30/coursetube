2b50c1153670f2d9aacbe8cc63160c243b5b6242
Nakul <nakulgarg1424@gmail.com>
2024-07-27 21:00:48 +0530

    ChapterList

---
+++ src/components/ChapterList.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { handleSeekChange } from "../config/playerSlice";
+import ListItem from "./ListItem";
+
+function ChapterList() {
+    const { chapters } = useSelector((state) => state.player.value);
+    const [chaptersList, setchaptersList] = useState([]);
+    const dispatch = useDispatch();
+
+    function playOnSeek(val) {
+        dispatch(
+            handleSeekChange({
+                playAt: chapters[parseInt(val)]["time"],
+                index: parseInt(val),
+            })
+        );
+    }
+    useEffect(() => {
+        var list = [];
+
+        for (var i in chapters) {
+            list.push(
+                <ListItem
+                    key={i}
+                    id={i}
+                    title={chapters[i]["title"]}
+                    played={chapters[i]["played"]}
+                    playOnSeek={playOnSeek}
+                />
+            );
+        }
+
+        setchaptersList(list);
+    }, []);
+
+  return (
+    <>
+    <div>{chaptersList}</div>
+    </>
+  )
+}
+
+export default ChapterList;
